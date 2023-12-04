d6c1deea87ddf49b9bc27d1e9f0a47231beb5fe4
Sean McOwen <seanmcowen@gmail.com>
2023-12-04 16:02:26 -0500

    Add new mechs

--- MSML/src/Mechanisms/Iteration.py
+++ MSML/src/Mechanisms/Iteration.py
@@ -1,8 +1,28 @@
-from ..Spaces import update_x_space
+from ..Spaces import update_x_space, dummy_space1
 
-update_x_mechanism = {"name": "Update X",
-                           "description": "Mechanism for update to the x value",
-                           "constraints": [],
-                           "logic": "",
-                           "domain": [update_x_space],
-                           "parameters_used": []}
+update_x_mechanism = {
+    "name": "Update X",
+    "description": "Mechanism for update to the x value",
+    "constraints": [],
+    "logic": "",
+    "domain": [update_x_space],
+    "parameters_used": [],
+}
+
+increment_iteration_step_mechanism = {
+    "name": "Increment Iteration Step Mechanism",
+    "description": "Mechanism for incrementing the iteration step by 1",
+    "constraints": [],
+    "logic": "state['iteration_step'] += 1",
+    "domain": [dummy_space1],
+    "parameters_used": [],
+}
+
+set_simulation_time_mechanism = {
+    "name": "Set Simulation Time Mechanism",
+    "description": "Mechanism for setting the current time of the simulation",
+    "constraints": [],
+    "logic": "state['simulation_time'] = now()",
+    "domain": [dummy_space1],
+    "parameters_used": [],
+}
--- MSML/src/Mechanisms/__init__.py
+++ MSML/src/Mechanisms/__init__.py
@@ -1,7 +1,15 @@
 from .Dummy import dummy_mechanism
-from .Iteration import update_x_mechanism
+from .Iteration import (
+    update_x_mechanism,
+    increment_iteration_step_mechanism,
+    set_simulation_time_mechanism,
+)
 from .Metrics import log_computation_time_metric_mechanism
 
-mechanism = {"Dummy Mechanism": dummy_mechanism,
-             "Update X Mechanism": update_x_mechanism,
-             "Log Computation Time Metric Mechanism": log_computation_time_metric_mechanism}
+mechanism = {
+    "Dummy Mechanism": dummy_mechanism,
+    "Update X Mechanism": update_x_mechanism,
+    "Log Computation Time Metric Mechanism": log_computation_time_metric_mechanism,
+    "Increment Iteration Step Mechanism": increment_iteration_step_mechanism,
+    "Set Simulation Time Mechanism": set_simulation_time_mechanism,
+}
