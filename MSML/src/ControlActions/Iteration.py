0d9031c053c427e93720bf7aaef86e176b42e0a5
SeanMcOwen <seanmcowen@gmail.com>
2024-01-17 19:46:38 -0500

    More cleanup

--- MSML/src/ControlActions/Iteration.py
+++ MSML/src/ControlActions/Iteration.py
@@ -1,10 +1,8 @@
-from ..Spaces import initialization_space
-
 initialization_control_action = {
     "name": "Initialization Control Action",
     "description": "Control action to begin the simulation",
     "constraints": [],
     "control_action_options": [],
-    "codomain": [initialization_space],
+    "codomain": ("Initialization Space",),
     "parameters_used": [],
 }
